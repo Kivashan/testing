415eb83071360a284e2ba72f68b2a7a3f2d7bba7
Albert <albertirura11@gmail.com>
2023-04-17 12:32:59 -0700

    shell

--- hsh.c
+++ hsh.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[], char *envp[])
 {
 	char *cmd;
-	int retval, retvale = 0, pid;
+	int retval, retvale = 0, pid, status;
 	size_t bytes = 1024;
 	char **tokens;
 	char *delim = " ";
@@ -15,28 +15,29 @@ int main(int argc, char *argv[], char *envp[])
 		int i = 0;
 
 		get_prompt();
-		/*cmd = malloc(sizeof(char) * bytes);
+		cmd = malloc(sizeof(char) * bytes);
 		if (!cmd)
 			return (-1);
-		retval = getline(&cmd, &bytes, stdin);*/
-		retval = our_getline(&cmd, &bytes, STDIN_FILENO);
+		retval = getline(&cmd, &bytes, stdin);
+	/*	retval = our_getline(&cmd, &bytes, STDIN_FILENO);*/
 		if (retval == -1)
 			_puts("Error reading command line");
-		/*cmd[stringlen(cmd) - 1] = '\0';*/
+		cmd[stringlen(cmd) - 1] = '\0';
 		tmp = stringcpy(cmd);
 		tokens = the_tokeniser(cmd, delim);
 		if (file_check(tokens, environ) == 0)
 		{
+			printf("This is the main process\n");
 			pid = fork();
-			if (pid == 0)
+			if (pid == -1)
+                        {
+                                perror("Error:");
+                                return (1);
+                        }
+			else if (pid == 0)
 				retvale = our_execve(tokens, environ);
-			else if (pid == -1)
-			{
-				perror("Error:");
-				return (1);
-			}
 			else
-				wait(&retvale);
+				wait(&status);
 		}
 		else
 			cmd_not_found_error(tokens[0]); /* error message to match linux */
