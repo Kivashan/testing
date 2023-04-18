3f004f4ef14a6d10273b40cb49758889cd890d4b
Kivashan <bvkivashan1982@gmail.com>
2023-04-18 06:00:45 -0700

    changes

--- hsh.c
+++ hsh.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
-int main(int argc, char *argv[], char *envp[])
+int main(int argc __attribute__((unused)),
+	       	char *argv[] __attribute__((unused)), char *envp[])
 {
-	char *cmd;
-	int retval, retvale = 0, pid, status;
-	size_t bytes = 1024;
+	char *cmd = NULL;
+	int retval, retvale = 0, pid, status, int_mode = 1;
+	size_t bytes;/* can later assign value if getline does not work properly */
 	char **tokens;
 	char *delim = " ";
 	extern char **environ;
@@ -12,36 +13,66 @@ int main(int argc, char *argv[], char *envp[])
 
 	while(1)
 	{
-		int i = 0;
-
-		get_prompt();
-		cmd = malloc(sizeof(char) * bytes);
-		if (!cmd)
-			return (-1);
-		retval = getline(&cmd, &bytes, stdin);
-	/*	retval = our_getline(&cmd, &bytes, STDIN_FILENO);*/
-		if (retval == -1)
-			_puts("Error reading command line");
-		cmd[stringlen(cmd) - 1] = '\0';
-		tmp = stringcpy(cmd);
-		tokens = the_tokeniser(cmd, delim);
-		if (file_check(tokens, environ) == 0)
+		int_mode = isatty(STDIN_FILENO);
+		if (int_mode)
 		{
-			printf("This is the main process\n");
-			pid = fork();
-			if (pid == -1)
-                        {
-                                perror("Error:");
-                                return (1);
-                        }
-			else if (pid == 0)
-				retvale = our_execve(tokens, environ);
+			int i = 0;
+			get_prompt();
+			/*cmd = malloc(sizeof(char) * bytes);
+			if (!cmd)
+				return (-1);*/
+			retval = getline(&cmd, &bytes, stdin);
+	/*		retval = our_getline(&cmd, &bytes, STDIN_FILENO);*/
+			if (retval == -1)
+			{
+				free(cmd);
+				exit(98);
+			}
+			cmd[stringlen(cmd) - 1] = '\0';
+			tokens = the_tokeniser(cmd, delim);
+			if (file_check(tokens, environ) == 0)
+			{
+				pid = fork();
+				if (pid == -1)
+				{
+					perror("Error:");
+					return (1);
+				}
+				else if (pid == 0)
+					retvale = our_execve(tokens, environ);
+				else
+					wait(&status);
+			}
 			else
-				wait(&status);
+				cmd_not_found_error(tokens[0]); /* error message to match linux */
 		}
 		else
-			cmd_not_found_error(tokens[0]); /* error message to match linux */
+		{
+			retval = getline(&cmd, &bytes, stdin);
+			/*retval = our_getline(&cmd, &bytes, STDIN_FILENO);*/
+			if (retval == -1)
+			{
+				free(cmd);
+				exit(98);
+			}
+			cmd[stringlen(cmd) - 1] = '\0';
+			tokens = the_tokeniser(cmd, delim);
+			if (file_check(tokens, environ) == 0)
+			{
+				pid = fork();
+				if (pid == -1)
+				{
+					perror("Error:");
+					return (1);
+				}
+				else if (pid == 0)
+					retvale = our_execve(tokens, environ);
+				else
+					wait(&status);
+			}
+			else
+				cmd_not_found_error(tokens[0]);
+		}
 	}
-
 	return (0);
 }
